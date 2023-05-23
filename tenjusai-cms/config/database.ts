356cee6ab9eb2837ba84ddf3614d17cb311bc43c
nishide-dev <nishide.dev@gmail.com>
2023-05-23 15:19:17 +0900

    ✅ Dockerfile作成

--- tenjusai-cms/config/database.ts
+++ tenjusai-cms/config/database.ts
@@ -1,4 +1,5 @@
 import path from 'path';
+import { readFileSync } from "node:fs";
 
 export default ({ env }) => {
   const client = env('DATABASE_CLIENT', 'sqlite');
@@ -55,17 +56,24 @@ export default ({ env }) => {
         database: env('DATABASE_NAME', 'strapi'),
         user: env('DATABASE_USERNAME', 'strapi'),
         password: env('DATABASE_PASSWORD', 'strapi'),
-        ssl: env.bool('DATABASE_SSL', false) && {
-          key: env('DATABASE_SSL_KEY', undefined),
-          cert: env('DATABASE_SSL_CERT', undefined),
-          ca: env('DATABASE_SSL_CA', undefined),
-          capath: env('DATABASE_SSL_CAPATH', undefined),
-          cipher: env('DATABASE_SSL_CIPHER', undefined),
-          rejectUnauthorized: env.bool(
-            'DATABASE_SSL_REJECT_UNAUTHORIZED',
-            true
-          ),
-        },
+        // ssl: env.bool('DATABASE_SSL', false) && {
+        //   key: env('DATABASE_SSL_KEY', undefined),
+        //   cert: env('DATABASE_SSL_CERT', undefined),
+        //   ca: env('DATABASE_SSL_CA', undefined),
+        //   capath: env('DATABASE_SSL_CAPATH', undefined),
+        //   cipher: env('DATABASE_SSL_CIPHER', undefined),
+        //   rejectUnauthorized: env.bool(
+        //     'DATABASE_SSL_REJECT_UNAUTHORIZED',
+        //     true
+        //   ),
+        // },
+        ssl: env.bool("DATABASE_SSL")
+        ? {
+            ca: readFileSync(
+              `${__dirname}/../../supabase.crt`
+            ).toString(),
+          }
+        : false,
         schema: env('DATABASE_SCHEMA', 'public'),
       },
       pool: { min: env.int('DATABASE_POOL_MIN', 2), max: env.int('DATABASE_POOL_MAX', 10) },
