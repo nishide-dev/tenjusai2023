185dc16c2f3a161d217ab3acec8508a278570c32
nishide-dev <nishide.dev@gmail.com>
2023-06-22 09:17:34 +0900

    ✅ s3 upload plugin

--- tenjusai-cms/config/middlewares.ts
+++ tenjusai-cms/config/middlewares.ts
@@ -1,6 +1,32 @@
-export default [
+module.exports = ({ env }) => [
+  {
+    name: 'strapi::security',
+    config: {
+      contentSecurityPolicy: {
+        useDefaults: true,
+        directives: {
+          'connect-src': ["'self'", 'https:'],
+          'img-src': [
+            "'self'",
+            'data:',
+            'blob:',
+            'dl.airtable.com',
+            'tenjusai2023-cms.s3.ap-northeast-1.amazonaws.com',
+          ],
+          'media-src': [
+            "'self'",
+            'data:',
+            'blob:',
+            'dl.airtable.com',
+            'tenjusai2023-cms.s3.ap-northeast-1.amazonaws.com',
+          ],
+          upgradeInsecureRequests: null,
+        },
+      },
+    },
+  },
   'strapi::errors',
-  'strapi::security',
+  // 'strapi::security',
   'strapi::cors',
   'strapi::poweredBy',
   'strapi::logger',
