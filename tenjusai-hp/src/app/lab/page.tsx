0a4258486d46f5a6411bc72310e7fbe10daea591
nishide-dev <nishide.dev@gmail.com>
2023-07-05 17:16:22 +0900

    ✅ stage, lab init

--- tenjusai-hp/src/app/lab/page.tsx
+++ tenjusai-hp/src/app/lab/page.tsx
@@ -1,58 +1,10 @@
 'use client';
-import {useEffect, useState, useRef } from 'react';
+import { useState } from "react";
 
-const Lab: React.FC = () => {
-
-  const containerRef = useRef<HTMLDivElement>(null);
-
-  useEffect(() => {
-    let lastScrollTop = 0;
-
-    const handleScroll = () => {
-      const container = containerRef.current;
-      if (!container) {
-        return;
-      }
-
-      const st = container.scrollTop;
-
-      if (st > lastScrollTop) {
-        // downscroll
-        container.classList.add("snap-y");
-        container.classList.add("snap-mandatory");
-      } else {
-        // upscroll
-        container.classList.remove("snap-y");
-        container.classList.remove("snap-mandatory");
-      }
-      lastScrollTop = st;
-    };
-
-    const container = containerRef.current;
-    if (container) {
-      container.addEventListener("scroll", handleScroll);
-    }
-
-    return () => {
-      if (container) {
-        container.removeEventListener("scroll", handleScroll);
-      }
-    };
-  }, []);
-
-  return (
-    <>
-    <div className="relative">
-      <div ref={containerRef} className="snap-y snap-mandatory h-screen w-screen overflow-scroll scrollbar-hide">
-        <div className="snap-start h-screen w-screen bg-red-500">1</div>
-        <div className="snap-start h-screen w-screen bg-green-500">2</div>
-        <div className="snap-start h-screen w-screen bg-blue-500">3</div>
-      </div>
-    </div>
-
-    {/* <div className="h-screen bg-tenjusaiHero"></div> */}
-    </>
-  );
+export default function Lab() {
+    return (
+        <div className="flex flex-col items-center justify-center h-screen">
+          <h2>研究室</h2>
+        </div>
+    )
 }
-
-export default Lab;
---
+++ tenjusai-hp/src/app/stage_/page.tsx
@@ -0,0 +1,62 @@
+'use client';
+import { useEffect, useState } from 'react';
+import type { WheelEvent } from 'react';
+
+const Stage: React.FC = () => {
+  const [radius, setRadius] = useState(0);
+
+  useEffect(() => {
+    // スクロールを制御する
+    const controlScroll: EventListener = (e) => {
+      const wheelEvent = e as unknown as WheelEvent;
+      if ((radius < 2000 && wheelEvent.deltaY > 0) || (radius > 0 && wheelEvent.deltaY < 0)) { // 2000は半径の最大値、0は半径の最小値です
+        e.preventDefault();
+      }
+    };
+    document.body.addEventListener('wheel', controlScroll, { passive: false });
+
+    // アニメーションを制御する
+    const handleScroll: EventListener = (e) => {
+      const wheelEvent = e as unknown as WheelEvent;
+      setRadius((prevRadius) => {
+        const newRadius = Math.max(0, Math.min(prevRadius + wheelEvent.deltaY * 0.5, 2000)); // 2000 は最大半径, 0 は最小半径です。値を調整してください。
+        return newRadius;
+      });
+    };
+    window.addEventListener('wheel', handleScroll);
+
+    // イベントリスナーをクリーンアップ
+    return () => {
+      document.body.removeEventListener('wheel', controlScroll);
+      window.removeEventListener('wheel', handleScroll);
+    };
+  }, [radius]);
+
+  
+
+  return (
+    <div className="min-h-screen bg-blue-100 overflow-hidden">
+      <div
+        className="absolute inset-0"
+        style={{
+          clipPath: `circle(${radius}px at center)`,
+          backgroundColor: 'white',
+          transition: 'clip-path 0.5s ease-in-out',
+        }}
+      >
+        <div className="pt-64 px-8 text-center">
+          <h1 className="text-4xl font-bold mb-4">Scroll Down</h1>
+          <p className="text-lg">Watch the circle expand and reveal content below</p>
+        </div>
+        <div className="mt-64 px-8 text-center">
+          <h2 className="text-3xl font-bold mb-4">Content Below</h2>
+          <p className="text-lg">This content is revealed as the circle expands.</p>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Stage;
+
+
