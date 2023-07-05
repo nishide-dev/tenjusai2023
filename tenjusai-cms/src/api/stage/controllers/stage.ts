d62f782b41c111bfbaafc7ffce0ec1267f2d83fc
nishide-dev <nishide.dev@gmail.com>
2023-07-05 16:46:37 +0900

    ⚡️ cms途中

---
+++ tenjusai-cms/src/api/stage/controllers/stage.ts
@@ -0,0 +1,7 @@
+/**
+ * stage controller
+ */
+
+import { factories } from '@strapi/strapi'
+
+export default factories.createCoreController('api::stage.stage');
---
+++ tenjusai-cms/src/api/stage/services/stage.ts
@@ -0,0 +1,7 @@
+/**
+ * stage service
+ */
+
+import { factories } from '@strapi/strapi';
+
+export default factories.createCoreService('api::stage.stage');
