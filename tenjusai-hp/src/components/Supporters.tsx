061b044d14b7a7d1545fb4d440ddf887a92d1b9c
nishide-dev <nishide.dev@gmail.com>
2023-08-13 07:55:59 +0900

    ⚡️ Supporter 修正

--- tenjusai-hp/src/components/Supporters.tsx
+++ tenjusai-hp/src/components/Supporters.tsx
@@ -17,27 +17,48 @@ const Supporters: React.FC = () => {
                 <div className="container p-4 mx-auto text-center">
                     <h2 className="text-4xl font-bold text-gray-600">Supporters</h2>
                 </div>
-                <div className="container flex flex-wrap justify-center mx-auto my-5">
+                <div className="container flex flex-wrap justify-center mx-auto my-6">
                     {
-                        supporters.map((supporter, index) => (
-                            <div key={index} className="flex items-center justify-center w-full p-3 md:p-3 align-middle md:w-1/2 xl:w-1/3 2xl:w-1/4">
-                                <FadeInToUp className="">
-                                    <Image src={supporter.thumbnail_link} alt={supporter.name} height={600} width={600} className="object-contain max-h-8 md:max-h-10 lg:max-h-12 xl:max-h-14" />
-                                </FadeInToUp>
-                            </div>
-                        ))
+                        supporters.map((supporter, index) => {
+                            return (
+                                <div key={index} className="flex items-center justify-center w-full p-3 md:p-3 align-middle md:w-1/2 xl:w-1/3 2xl:w-1/4">
+                                    <FadeInToUp className="">
+                                        <Image src={supporter.thumbnail_link} alt={supporter.name} height={600} width={600} className="object-contain max-h-10 md:max-h-12 lg:max-h-14 xl:max-h-16" />
+                                    </FadeInToUp>
+                                </div>
+                            )
+                        })
                     }
+                </div>
+                <div className="mt-5 flex gap-5">
+                    <div className="container flex flex-wrap justify-center mx-auto my-5">
+                        {
+                            banners.map((banner, index) => {
+                                if (!banner.link) {
+                                    return (
+                                        <FadeInToUp key={banner.name}>
+                                            <Image src={banner.thumbnail_link} alt={banner.alt} height={400} width={400} />
+                                        </FadeInToUp>
+                                    )
+                                }}
+                            )
+                        }
+                    </div>
                 </div>
                 <div className="mt-20 flex gap-5">
                     <div className="container flex flex-wrap justify-center mx-auto my-5">
                         {
-                            banners.map((banner, index) => (
-                                <Link href={banner.link || ""} key={banner.name} className="" >
-                                    <FadeInToUp>
-                                        <Image src={banner.thumbnail_link} alt={banner.alt} height={600} width={600} />
-                                    </FadeInToUp>
-                                </Link>
-                            ))
+                            banners.map((banner, index) => {
+                                if (banner.link) {
+                                    return (
+                                        <Link href={banner.link || ""} key={banner.name} className="" >
+                                            <FadeInToUp>
+                                                <Image src={banner.thumbnail_link} alt={banner.alt} height={600} width={600} />
+                                            </FadeInToUp>
+                                        </Link>
+                                    )
+                                }
+                            })
                         }
                     </div>
                 </div>
