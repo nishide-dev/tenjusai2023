290e1ff71c3e10e9870b20103933368c84d5c1d0
nishide-dev <nishide.dev@gmail.com>
2023-08-17 20:52:19 +0900

    ⚡️ test

--- tenjusai-analytics/src/app/page.tsx
+++ tenjusai-analytics/src/app/page.tsx
@@ -22,52 +22,52 @@ export default function Home() {
           <TableBody>
             <TableRow key="1">
               <TableCell>本番環境</TableCell>
-              <TableCell>{analytics?.production[0].domain || ""}</TableCell>
+              <TableCell>{analytics?.production[0].domain}</TableCell>
               <TableCell>{analytics?.production.length}</TableCell>
             </TableRow>
             <TableRow key="2">
               <TableCell>開発環境</TableCell>
-              <TableCell>{analytics?.development[0].domain || ""}</TableCell>
+              <TableCell>{analytics?.development[0].domain}</TableCell>
               <TableCell>{analytics?.development.length}</TableCell>
             </TableRow>
             <TableRow key="3">
               <TableCell>B1</TableCell>
-              <TableCell>{analytics?.b1[0].domain || ""}</TableCell>
+              <TableCell>{analytics?.b1[0].domain}</TableCell>
               <TableCell>{analytics?.b1.length}</TableCell>
             </TableRow>
             <TableRow key="4">
             <TableCell>B2</TableCell>
-            <TableCell>{analytics?.b2[0].domain || ""}</TableCell>
+            <TableCell>{analytics?.b2[0].domain}</TableCell>
               <TableCell>{analytics?.b2.length}</TableCell>
             </TableRow>
             <TableRow key="5">
             <TableCell>B3</TableCell>
-            <TableCell>{analytics?.b3[0].domain || ""}</TableCell>
+            <TableCell>{analytics?.b3[0].domain}</TableCell>
               <TableCell>{analytics?.b3.length}</TableCell>
             </TableRow>
             <TableRow key="6">
             <TableCell>B4</TableCell>
-            <TableCell>{analytics?.b4[0].domain || ""}</TableCell>
+            <TableCell>{analytics?.b4[0].domain}</TableCell>
               <TableCell>{analytics?.b4.length}</TableCell>
             </TableRow>
             <TableRow key="7">
             <TableCell>M1</TableCell>
-            <TableCell>{analytics?.m1[0].domain || ""}</TableCell>
+            <TableCell>{analytics?.m1[0].domain}</TableCell>
               <TableCell>{analytics?.m1.length}</TableCell>
             </TableRow>
             <TableRow key="8">
             <TableCell>M2</TableCell>
-            <TableCell>{analytics?.m2[0].domain || ""}</TableCell>
+            <TableCell>{analytics?.m2[0].domain}</TableCell>
               <TableCell>{analytics?.m2.length}</TableCell>
             </TableRow>
             <TableRow key="9">
             <TableCell>職員</TableCell>
-            <TableCell>{analytics?.shokuin[0].domain || ""}</TableCell>
+            <TableCell>{analytics?.shokuin[0].domain}</TableCell>
               <TableCell>{analytics?.shokuin.length}</TableCell>
             </TableRow>
             <TableRow key="10">
             <TableCell>Classroom</TableCell>
-            <TableCell>{analytics?.classroom[0].domain || ""}</TableCell>
+            <TableCell>{analytics?.classroom[0].domain}</TableCell>
               <TableCell>{analytics?.classroom.length}</TableCell>
             </TableRow>
           </TableBody>
