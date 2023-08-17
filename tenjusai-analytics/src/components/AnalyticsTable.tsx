af883321d0721c4fab9987334d1d53137b0eb908
nishide-dev <nishide.dev@gmail.com>
2023-08-17 23:21:26 +0900

    ⚡️ test

--- tenjusai-analytics/src/components/AnalyticsTable.tsx
+++ tenjusai-analytics/src/components/AnalyticsTable.tsx
@@ -4,7 +4,7 @@ import Link from 'next/link';
 import TableGrid from './TableGrid';
 
 async function getData() {
-    const res = await fetch("https://tenjusai2023-analytics/api/v1/analytics", {cache: "no-cache"});
+    const res = await fetch("/api/v1/analytics", {cache: "no-cache"});
    
     if (!res.ok) {
       throw new Error('Failed to fetch data')
