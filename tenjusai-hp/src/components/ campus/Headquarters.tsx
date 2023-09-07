1ed4f7a3713acfacb3c871ff0e838b48c9732ac2
nishide-dev <nishide.dev@gmail.com>
2023-09-07 15:41:49 +0900

    ✨ キャンパスマップの導入

--- tenjusai-hp/src/components/ campus/Headquarters.tsx
+++ tenjusai-hp/src/components/ campus/Headquarters.tsx
@@ -12,11 +12,14 @@ const Headquarters: React.FC = () => {
                         <p className="text-base font-semibold leading-7 text-tenjusaiGreen">Campus Map</p>
                         <h1 className="mt-2 text-3xl font-bold tracking-tight text-gray-700 sm:text-4xl">校内マップ</h1>
                         <div id="headquarters" className="mt-12">
-                            {/* <div className="font-semibold text-3xl text-gray-700">本部</div>
-                            <div className="my-6">
-                                <Image src="/campus/headquarters.jpg" className="rounded-md" alt="本部" width={600} height={600} />
-                            </div> */}
-                            <div className="text-tenjusaiGreen font-bold text-2xl">準備中...</div>
+                            {/* <div className="font-semibold text-3xl text-gray-700">本部</div> */}
+                            <div className="my-6 hidden sm:block">
+                                <Image src="/campus/campus_pc2.png" className="rounded-md" alt="本部" width={1200} height={1200} />
+                            </div>
+                            <div className="my-6 sm:hidden">
+                                <Image src="/campus/campus_mobile3.png" className="rounded-md" alt="本部" width={1200} height={1200} />
+                            </div>
+                            {/* <div className="text-tenjusaiGreen font-bold text-2xl">準備中...</div> */}
                         </div>
                     </div>
                 </FadeIn>
