e32b3a57b0d771d647ca45f1b653c792d6746a92
nishide-dev <nishide.dev@gmail.com>
2023-09-07 15:48:53 +0900

    ⚡️ link to a

--- tenjusai-hp/src/app/page.tsx
+++ tenjusai-hp/src/app/page.tsx
@@ -95,12 +95,12 @@ export default function Home() {
               {/* <div className=""><h1 className="text-tenjusaiOrange text-9xl font-extrabold">TENJUSAI</h1></div> */}
               <p className="font-semibold text-gray-500 text-lg md:text-xl lg:text-2xl mt-2">40th 豊田工業大学 大学祭</p>
               <div className="m-2 mt-4 font-bold text-tenjusaiOrange text-2xl md:text-3xl lg:text-4xl">9/9 Sat 9/10 Sun</div>
-              <Link href="https://tenjusai2023-cms.s3.ap-northeast-1.amazonaws.com/2023.pdf">
+              <a href="https://tenjusai2023-cms.s3.ap-northeast-1.amazonaws.com/2023.pdf">
                 <button type="button" className="mt-7 text-white bg-tenjusaiOrange hover:bg-tenjusaiOrange focus:ring-2 focus:outline-none focus:ring-tenjusaiOrange font-semibold rounded-2xl text-md px-5 py-3.5 text-center inline-flex items-center">
                     パンフレットはこちら
                     <svg aria-hidden="true" className="w-5 h-5 ml-2 -mr-1" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg"><path fillRule="evenodd" d="M10.293 3.293a1 1 0 011.414 0l6 6a1 1 0 010 1.414l-6 6a1 1 0 01-1.414-1.414L14.586 11H3a1 1 0 110-2h11.586l-4.293-4.293a1 1 0 010-1.414z" clipRule="evenodd"></path></svg>
                 </button>
-              </Link>
+              </a>
             </div>
           </div>
         </div>
@@ -110,12 +110,12 @@ export default function Home() {
           {/* <div className=""><h1 className="text-tenjusaiOrange text-6xl font-extrabold sm:text-7xl md:text-8xl">TENJUSAI</h1></div> */}
           <p className="font-semibold text-gray-500">40th 豊田工業大学 大学祭</p>
           <div className="m-2 mt-4 font-bold text-tenjusaiOrange text-3xl">9/9 Sat 9/10 Sun</div>
-          <Link href="https://tenjusai2023-cms.s3.ap-northeast-1.amazonaws.com/2023.pdf">
+          <a href="https://tenjusai2023-cms.s3.ap-northeast-1.amazonaws.com/2023.pdf">
             <button type="button" className="mt-7 text-white bg-tenjusaiOrange hover:bg-tenjusaiOrange focus:ring-2 focus:outline-none focus:ring-tenjusaiOrange font-semibold rounded-2xl text-md px-5 py-3 text-center inline-flex items-center">
                 パンフレットはこちら
                 <svg aria-hidden="true" className="w-5 h-5 ml-2 -mr-1" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg"><path fillRule="evenodd" d="M10.293 3.293a1 1 0 011.414 0l6 6a1 1 0 010 1.414l-6 6a1 1 0 01-1.414-1.414L14.586 11H3a1 1 0 110-2h11.586l-4.293-4.293a1 1 0 010-1.414z" clipRule="evenodd"></path></svg>
             </button>
-          </Link>
+          </a>
         </div>
       </div>
       <AboutUs />
